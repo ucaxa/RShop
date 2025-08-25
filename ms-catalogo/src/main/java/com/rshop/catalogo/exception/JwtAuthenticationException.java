263d9a9ab3badfd14424bba35de9bc6ad145d2e9
Reinaldo Silva <rein.ant@gmail.com>
2025-08-25 13:45:25 -0300

    remoção de duplicidade de autenticacao

--- ms-catalogo/src/main/java/com/rshop/catalogo/exception/JwtAuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.rshop.catalogo.exception;
-
-public class JwtAuthenticationException extends RuntimeException {
-    public JwtAuthenticationException(String message) {
-        super(message);
-    }
-}
